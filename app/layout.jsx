07d9455565d08be4a6a8cbd665546c7c67357fbf
Aminu Muhammad <aminuastroboy@gmail.com>
2026-03-15 03:35:01 +0100

    Add files via upload

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,14 @@
+import "./globals.css";
 
-export default function RootLayout({children}){
-return(
-<html>
-<body>{children}</body>
-</html>
-)
+export const metadata = {
+  title: "ASAJ Full UI Campaign Website",
+  description: "Official campaign website for Hon. Abdulkareem Sabo Abdullahi (ASAJ)",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
